d270482d0e632290633a82fb1cb7dfce19baf7ee
zoooo-hs <dogfooter219@gmail.com>
2022-01-12 23:16:56 +0900

    init

---
+++ src/main/java/com/zoooohs/instagramclone/InstagramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.zoooohs.instagramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoooohs/instagramclone/InstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoooohs.instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
