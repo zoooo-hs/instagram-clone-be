4caf4b108d02808bd88fcd9ab048005efe8708b2
zoooo-hs <dogfooter219@gmail.com>
2022-02-24 23:54:51 +0900

    feat: swagger ui Auth 추가하여, 인증 필요한 API 테스트 가능

--- src/main/java/com/zoooohs/instagramclone/configuration/OpenAPIConfiguration.java
+++ src/main/java/com/zoooohs/instagramclone/configuration/OpenAPIConfiguration.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class OpenAPIConfiguration {
@@ -24,8 +28,13 @@ public class OpenAPIConfiguration {
                 .description("퇴근하고 뭐라도 해보려고 한다. TDD 공부도 하고 Spring 프로젝트를 계속 손에 익혀본다.")
                 .contact(new Contact().name("zoooo-hs (Hyunsu Ju)").url("https://github.com/zoooo-hs").email("dogfooter219@gmail.com"));
 
+        SecurityScheme securityScheme = new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .scheme("bearer").bearerFormat("JWT").in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(info);
     }
 }
