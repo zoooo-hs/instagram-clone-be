99dd24c0cfb08dafe64cbf52812a52bed271382e
zoooo-hs <dogfooter219@gmail.com>
2022-03-10 23:40:18 +0900

    fix: local profile 한정 CORS allows all 적용

---
+++ src/main/java/com/zoooohs/instagramclone/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package com.zoooohs.instagramclone.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("local")
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+
+}
